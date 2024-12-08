90aa48c2121e830e95bd2f68cb7b7d66ec507c5d
Gouthami V N <gouthamivn2@gmail.com>
2024-12-08 19:45:07 +0530

    added file

---
+++ FindandReplaceSubstring.java
@@ -0,0 +1,17 @@
+#Find and Replace Substring
+package session_72_08_12_2024;
+public class FindandReplaceSubstring {
+		    public static void main(String[] args) {
+		        String original = "Hello world! Welcome to the world of Java.";
+		        String toFind = "world";
+		        String toReplace = "universe";
+		        StringBuffer sb = new StringBuffer(original);
+		        int index = sb.indexOf(toFind); 
+		        while (index != -1) {
+		            sb.replace(index, index + toFind.length(), toReplace);
+		            index = sb.indexOf(toFind, index + toReplace.length());
+		        }
+		        System.out.println("Modified string: " + sb.toString());
+	}
+
+}
